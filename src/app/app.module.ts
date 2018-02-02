2fd15416648193dfaf4af8cadfb5148fc53fd3ff
omadoyeabraham <omadoyeabraham@gmail.com>
2018-02-02 13:43:08 +0100

    deleted module files for home, login and dashboard pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from "@angular/platform-browser";
+import { ErrorHandler, NgModule } from "@angular/core";
+import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { ReactiveFormsModule } from "@angular/forms";
 
-import { MyApp } from './app.component';
+import { MyApp } from "./app.component";
+import { LoginPage } from "./appModule/pages/login/login";
+import { HomePage } from "./appModule/pages/home/home";
+import { DashboardPage } from "./appModule/pages/dashboard/dashboard";
 
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from "@ionic-native/status-bar";
+import { SplashScreen } from "@ionic-native/splash-screen";
 
 @NgModule({
-  declarations: [
-    MyApp,
-  ],
-  imports: [
-    ReactiveFormsModule,
-    BrowserModule,
-    IonicModule.forRoot(MyApp),
-  ],
+  declarations: [MyApp, HomePage, LoginPage, DashboardPage],
+  imports: [ReactiveFormsModule, BrowserModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [
-    MyApp,
-  ],
+  entryComponents: [MyApp],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler }
   ]
 })
 export class AppModule {}
