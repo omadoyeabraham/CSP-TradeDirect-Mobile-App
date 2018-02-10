77ad8c214b2c8f2ff7c2fd5a8727ad12a8a1ffed
omadoyeabraham <omadoyeabraham@gmail.com>
2018-02-10 20:54:08 +0100

    fixed the openPage() in the app component

--- src/app/stock-broking/pages/stb-container/stb-container.ts
+++ src/app/stock-broking/pages/stb-container/stb-container.ts
@@ -1,25 +1,19 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
 
 /**
- * Generated class for the StbContainerPage page.
  *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
+ *
+ * @export
+ * @class StbContainerPage
  */
-
 @IonicPage()
 @Component({
-  selector: 'page-stb-container',
-  templateUrl: 'stb-container.html',
+  selector: "page-stb-container",
+  templateUrl: "stb-container.html"
 })
 export class StbContainerPage {
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad StbContainerPage');
-  }
-
+  ionViewDidLoad() {}
 }
