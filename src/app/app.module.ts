7e7ba62f5d7d8409b1b675279286893c726a652d
omadoyeabraham <omadoyeabraham@gmail.com>
2018-04-09 10:59:30 +0100

    fixed issue where background image doesnt show on the dashboard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,20 @@ import { MyApp } from "./app.component";
 import { SharedModule } from "./sharedModule/shared.module";
 import { CspStoreModule } from "./store/store.module";
 
+/**
+ * The STB, FI and CASH modules were originally designed to be lazy loaded feature modules,
+ * but I am unhappy with the 1 second "ish" lag that users experience when opening pages in these
+ * modules for the first time. Hence the decision to eager load them here, and see if the increase
+ * in intial load time (caused by increased size of resources loaded eagerly) is an acceptable trade-off
+ * as long as the users subsequent experience is much smoother.
+ *
+ * RE: The app experience is much smoother, and the initial app load, did not really increase that much,
+ * so I'm leaving these modules as eager loaded feature modules. :)
+ */
+import { StockbrokingModule } from "./stockbrokingModule/stockbroking.module";
+import { FixedIncomeModule } from "./fixedIncomeModule/fixedIncome.module";
+import { CashModule } from "./cashModule/cash.module";
+
 import { rootReducer, metaReducers } from "./store/reducers";
 import { allEffects } from "./store/effects";
 import { TokenInterceptor } from "./sharedModule/services/auth/token.interceptor";
@@ -23,6 +37,9 @@ import { TokenInterceptor } from "./sharedModule/services/auth/token.interceptor
   imports: [
     SharedModule.forRoot(),
     CspStoreModule.forRoot(),
+    StockbrokingModule.forRoot(),
+    FixedIncomeModule,
+    CashModule,
     ReactiveFormsModule,
     BrowserModule,
     HttpClientModule,
