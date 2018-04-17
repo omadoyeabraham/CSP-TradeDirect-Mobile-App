a35a8ac4df8996e0bc676a053066da539e8b63d3
omadoyeabraham <omadoyeabraham@gmail.com>
2018-04-17 15:45:50 +0100

    added pull to refresh for portfolio holdings

--- src/app/stockbrokingModule/pages/holdings/portfolio-holdings-container/stb-portfolio-holdings-container.ts
+++ src/app/stockbrokingModule/pages/holdings/portfolio-holdings-container/stb-portfolio-holdings-container.ts
@@ -2,9 +2,16 @@ import { Component } from "@angular/core";
 import { IonicPage, NavController, NavParams } from "ionic-angular";
 import { Store } from "@ngrx/store";
 
-import { IAppState } from "../../../../store/models";
+import {
+  getUserState,
+  getActivePortfolio,
+  AuthActionDispatcher
+} from "../../../../store";
+import { IAppState, IUserState } from "../../../../store/models";
 import * as selectors from "../../../../store/selectors";
 import { IPortfolioHolding } from "../../../models/portfolioHolding.interface";
+import { IPortfolio } from "../../../models";
+import { AuthProvider } from "../../../../sharedModule/services/auth/auth";
 
 /**
  * Container component for stockbroking portfolio holdings
@@ -22,14 +29,19 @@ import { IPortfolioHolding } from "../../../models/portfolioHolding.interface";
 export class StbPortfolioHoldingsContainerPage {
   public activePortfolioStockHoldings: IPortfolioHolding[];
   public activePortfolioBondHoldings: IPortfolioHolding[];
+  public user: IUserState;
 
   constructor(
     public navCtrl: NavController,
     public navParams: NavParams,
-    private store: Store<IAppState>
+    private store: Store<IAppState>,
+    private authProvider: AuthProvider,
+    private authActionDispatcher: AuthActionDispatcher
   ) {}
 
   ionViewDidLoad() {
+    this.getUserState();
+
     // Subscribe to stock holdings of the currently selected portfolio
     this.store
       .select(selectors.getActivePortfolioStockHoldings)
@@ -43,5 +55,41 @@ export class StbPortfolioHoldingsContainerPage {
       .subscribe(bondHoldings => {
         this.activePortfolioBondHoldings = bondHoldings;
       });
+  }
+
+  getUserState() {
+    this.store
+      .select(getUserState)
+      .subscribe(userState => (this.user = userState));
+  }
+
+  /**
+   * Refresh the user's stockbroking data
+   *
+   * @param {any} refresher
+   * @memberof StbTradeHistoryContainerPage
+   */
+  doRefresh(refresher) {
+    // Update the user data
+    this.authProvider.getUserData(this.user.id).subscribe(
+      response => {
+        let activePortfolioID: number;
+        this.store
+          .select(getActivePortfolio)
+          .subscribe((portfolio: IPortfolio) => {
+            activePortfolioID = portfolio.id;
+          });
+
+        this.authActionDispatcher.updateUserDataInStore(
+          response,
+          activePortfolioID
+        );
+        refresher.complete();
+      },
+      err => {
+        console.log(err);
+        refresher.cancel();
+      }
+    );
   }
 }
